ff199191024546ef1794c3920878f17f997d027c
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-29 11:32:19 -0500

    started creating main page

---
+++ client/src/pages/home.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Home(){
+    const [books, updateBooks] = useState([]);
+    const [searchCriteria, updateCriteria] = useState('');
+    return(
+        
+    )
+}
--- controllers/googleAPI.js
+++ controllers/googleAPI.js
@@ -4,7 +4,7 @@ module.exports = {
     searchByParam: function(req, res){
        const {query: params} = req;
        makeRequest('https://www.googleapis.com/books/v1/volumes', params)
-       .then(() => )
+       .then(() => console.log('Not done yet!'))
        .catch(err => {
            res.status(422).send(err)
        })
