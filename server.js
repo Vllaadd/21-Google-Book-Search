5cf7a8331b9dbb43cfed53ec84fe536dffc44aa4
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-29 04:20:15 -0500

    connected to MongoDB

--- server.js
+++ server.js
@@ -10,6 +10,21 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+//===CONNECTING TO MONGODB======================================
+mongoose.connect('mongodb+srv://Vlad:columbia20mongo@cluster0.xm9q3.mongodb.net/booksearch?retryWrites=true&w=majority', 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+mongoose.connection.once('open', function(){
+  console.log('Connected to the database!');
+}).on('error', function(error){
+  console.log('Connection error:', error)
+})
+
+
 app.listen(PORT, () =>{
     console.log(`API Server now listening on PORT: http://localhost${PORT}`);
 });
