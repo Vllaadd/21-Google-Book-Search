8d91bc494eb41c78bdc3fb4db2f4a5345ea3f97f
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-29 04:23:52 -0500

    i required mongoose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //===DEPENDENCIES=======================
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -18,6 +19,7 @@ mongoose.connect('mongodb+srv://Vlad:columbia20mongo@cluster0.xm9q3.mongodb.net/
     useCreateIndex: true,
     useFindAndModify: false
 })
+
 mongoose.connection.once('open', function(){
   console.log('Connected to the database!');
 }).on('error', function(error){
