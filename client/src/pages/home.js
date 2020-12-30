27a3d063189f40ba6ceda47044db4793bcd9bc04
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-12-30 00:42:13 -0500

    home page created not finished

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,7 +4,49 @@ import axios from 'axios';
 function Home(){
     const [books, updateBooks] = useState([]);
     const [searchCriteria, updateCriteria] = useState('');
+
+    const handleSubmit =e => {
+        e.prevetnDefault();
+        axios
+        .get('/api/serch', {
+            params: {
+                q: searchCriteria
+            }
+        })
+        .then(data.json)
+    };
     return(
-        
+        <div>
+      <Navigation />
+
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <img
+            src="https://www.knowerstech.com/wp-content/uploads/2017/01/google-book.png"
+            className="large-google-logo"
+          />{" "}
+          <p className="lead sub-title-hp">
+            Find your favourite books using the google book API
+          </p>
+        </div>
+      </div>
+      <form onSubmit={e => handleSubmit(e)} className="hp-form">
+        <div className="form-group">
+          <input
+            placeholder="Search for books here"
+            type="input"
+            className="user-input"
+            id="input"
+            aria-describedby="emailHelp"
+            value={searchCriteria}
+            onChange={e => updateCriteria(e.target.value)}
+          />
+          <button type="submit" className="submit-button" role="submit">
+            FIND BOOKS
+          </button>
+        </div>
+      </form>
+      {books && <Books books={books} location="home" />}
+    </div>
     )
 }
