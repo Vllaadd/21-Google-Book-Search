ab40915e400e03c9801e0018faa22f0b613301c6
Vlad Zizic <vlladdiimiir@gmailc.om>
2021-01-06 02:01:09 -0500

    function split into smaller components

--- controllers/googleAPI.js
+++ controllers/googleAPI.js
@@ -1,12 +1,39 @@
 const db = require('../models');
+const makeRequest = require("../utils/makeRequest");
+
+const checkBookIntegrity = books => {
+  return books.filter(
+    book =>
+      book.id &&
+      book.volumeInfo.title &&
+      book.volumeInfo.infoLink &&
+      book.volumeInfo.authors &&
+      book.volumeInfo.description &&
+      book.volumeInfo.imageLinks &&
+      book.volumeInfo.imageLinks.thumbnail
+  );
+};
+
+const filterDuplicates = (newBooks, existingBooks) =>
+  newBooks.filter(newBook =>
+    existingBooks.every(
+      existingBook => existingBook.googleID.toString() !== newBook.id
+    )
+  );
 
 module.exports = {
-    searchByParam: function(req, res){
-       const {query: params} = req;
-       makeRequest('https://www.googleapis.com/books/v1/volumes', params)
-       .then(() => console.log('Not done yet!'))
-       .catch(err => {
-           res.status(422).send(err)
-       })
-    }
-}
+  searchByParam: function(req, res) {
+    const { query: params } = req;
+    makeRequest("https://www.googleapis.com/books/v1/volumes", params)
+      .then(({ data: { items: books } }) => checkBookIntegrity(books))
+      .then(newBooks =>
+        db.Book.find().then(existingBooks =>
+          filterDuplicates(newBooks, existingBooks)
+        )
+      )
+      .then(uniqueBooks => res.json(uniqueBooks))
+      .catch(err => {
+        res.status(422).send(err);
+      });
+  }
+};
