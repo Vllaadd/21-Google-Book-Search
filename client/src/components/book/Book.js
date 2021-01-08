926516bcf176a7b300b4578d51c38d87807992d2
Vlad Zizic <vlladdiimiir@gmailc.om>
2021-01-07 20:45:20 -0500

    save and delete buttons added

--- client/src/components/book/Book.js
+++ client/src/components/book/Book.js
@@ -7,6 +7,53 @@ const Book =({
     <div className="card-header book-title">{title}</div>
     <ul className="list-group list-group-flush">
       <li className="list-group-item book-author">{authors}</li>
+      <div className="image-desc-container">
+        <li className="description book-description">{description}</li>
+        <img src={thumbnail} className="thumbnail"></img>
+      </div>
+      <li className="list-group-item href">
+        <a href={infoLink}>{infoLink}</a>
+        {location === "home" ? (
+          <button
+            className="save-delete-button"
+            onClick={() => {
+              axios.post(
+                "/api/books",
+                {
+                  googleID,
+                  title,
+                  authors,
+                  description,
+                  thumbnail,
+                  link: infoLink
+                },
+                alert("Book now saved.")
+              );
+            }}
+          >
+            Save For Later
+          </button>
+        ) : (
+          <button
+            className="save-delete-button"
+            onClick={() => {
+              axios
+                .delete(
+                  "/api/books",
+                  {
+                    params: {
+                      id: _id
+                    }
+                  },
+                  alert("Book now removed.")
+                )
+                .then(() => refresh());
+            }}
+          >
+            Delete Book
+          </button>
+        )}
+      </li>
     </ul>
   </div>
 )
