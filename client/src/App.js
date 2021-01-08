054871f408dfee0ede64d42ca3bbc117143790f6
Vllaadd <vlladdiimiir@gmail.com>
2021-01-07 23:29:43 -0500

    app changed after adding page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './pages/home.js';
+import Home from './pages/saved.js';
 import Navigation from './components/nav.js';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Navigation />
-      <Home />
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </div>
     </Router>
   );
 }
