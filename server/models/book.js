8be89e2d5d9ebc99e05c7df920cbce3186da4260
Vllaadd <vlladdiimiir@gmail.com>
2021-01-24 04:14:36 -0500

    book schema changed

--- server/models/book.js
+++ server/models/book.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title: {type: String, required: true},
-    author: {type: [String]},
-    description:{type: String},
-    thumbnail: {type: String, required: true}
+    title: { type: String, required: true },
+    author: { type: [String] },
+    description: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    link: { type: String, required: true },
+    googleID: { type: String, required: true, unique: true }
 })
 
 const Book = mongoose.model('Book', bookSchema);
